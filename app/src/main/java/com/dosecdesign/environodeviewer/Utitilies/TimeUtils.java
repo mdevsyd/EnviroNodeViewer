497443233ed06ca37ee30e3b86ec8d06058853cf
Michelle Quaglia <michelle@dosec.com.au>
2017-05-19 16:19:00 +1000

    Add compareDates() to TimeUtils.

--- app/src/main/java/com/dosecdesign/environodeviewer/Utitilies/TimeUtils.java
+++ app/src/main/java/com/dosecdesign/environodeviewer/Utitilies/TimeUtils.java
@@ -1,5 +1,13 @@
 package com.dosecdesign.environodeviewer.Utitilies;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.dosecdesign.environodeviewer.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -23,5 +31,32 @@ public class TimeUtils {
         cal.setTime(oldDate);
         cal.add(Calendar.DATE, numDays);
         return cal.getTime();
+    }
+
+    /**
+     * compares two dates to check start is before end
+     * @param startDateTime user selected start date
+     * @param endDateTime user selected end date
+     * @return true if start is before end, false if not
+     */
+    public Boolean compareDates(String startDateTime, String endDateTime, Context c) {
+
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date start = sdf.parse(startDateTime);
+            Date end = sdf.parse(endDateTime);
+
+            if(start.before(end)){
+                Log.d(Constants.DEBUG_TAG,"date range valid");
+                return true;
+            }
+            else {
+                return false;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(c, R.string.try_again, Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
